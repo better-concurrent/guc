a3afdd3230d8462b0d8eec095a0771ba8ec981f0
goodplayer <meidomx@gmail.com>
2019-05-05 01:46:26 +0800

    priorityqueue add method test case

--- priorityqueue_test.go
+++ priorityqueue_test.go
@@ -22,3 +22,39 @@ func TestNewPriorityWithComparator(t *testing.T) {
 		t.Fatal("underlying comparator should not be nil")
 	}
 }
+
+type sampleItem struct {
+	Value int
+}
+
+func (this *sampleItem) Equals(i interface{}) bool {
+	dst, ok := i.(sampleItem)
+	if !ok {
+		return false
+	}
+	return this.Value == dst.Value
+}
+
+func (this *sampleItem) CompareTo(i interface{}) int {
+	return this.Value - i.(sampleItem).Value
+}
+
+func newSampleItem(value int) *sampleItem {
+	return &sampleItem{
+		Value: value,
+	}
+}
+
+func TestPriorityQueue_Add(t *testing.T) {
+	p := NewPriority()
+	b := p.Add(newSampleItem(10))
+	if !b {
+		t.Fatal("add result not true")
+	}
+	if len(p.data.data) != 1 {
+		t.Fatal("data len should be 1")
+	}
+	if p.data.data[0].(*sampleItem).Value != 10 {
+		t.Fatal("item value is not the origin value")
+	}
+}
